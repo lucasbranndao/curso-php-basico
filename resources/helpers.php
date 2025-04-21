be4e8252fac0c81be03764dc05a06cffe84e68b5
Lucas Brandão <lucasbranndao@gmail.com>
2025-04-21 13:26:48 -0300

    add helpers and mix config

---
+++ resources/helpers.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+
+if (!function_exists('base_path')) {
+    function base_path(string $path = ''): string
+    {
+        return dirname(__DIR__) . '/' . $path;
+    }
+}
+
+
+if (!function_exists('mix')) {
+    function mix(string $path): string
+    {
+
+        if (!file_exists(base_path('public/mix-manifest.json'))) {
+            throw new RuntimeException('Mix manifest not found. Run "npm run dev" or "npm run prod" to generate it.');
+        }
+
+
+
+        $content = file_get_contents(__DIR__ . '/../public/mix-manifest.json');
+        $content = json_decode($content, true);
+
+
+        return $content[$path] ?? '';
+    }
+}

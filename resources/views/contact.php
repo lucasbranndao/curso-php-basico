dd76097a99f6962aa581a0343d66ab86a585e0ba
Lucas Brandão <lucasbranndao@gmail.com>
2025-04-21 14:10:30 -0300

    ADD CONTACT VIEW FILE

---
+++ resources/views/contact.php
@@ -0,0 +1,20 @@
+<?php include __DIR__ . '/include/head.php' ?>
+<?php include __DIR__ . '/include/top-nav.php' ?>
+<?php include __DIR__ . '/include/header.php' ?>
+
+
+
+
+
+
+
+
+<main>
+    <div class=" mx-auto max-w-7xl py-5 px-8">
+        Contact content
+    </div>
+</main>
+
+
+
+<?php include __DIR__ . '/include/footer.php' ?>
---
+++ resources/views/include/header.php
@@ -0,0 +1,5 @@
+<header class="bg-white">
+    <div class="mx-auto max-w-7xl py-5 px-8">
+        <h1 class="text-3xl leading-9 font-bold text-gray-900""><?= $heading ?></h1>
+    </div>
+</header>

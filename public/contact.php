d70c103683f7aa1416235872e3f1173c351d9fd0
Lucas Brandão <lucasbranndao@gmail.com>
2025-04-21 13:31:35 -0300

    adds contact page

---
+++ public/contact.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../resources/helpers.php';
+
+use App\Models\Product;
+
+$products = Product::all();
+$filteredProducts = array_filter($products,  static fn($product) => $product['is_available']);
+
+$title = 'Contact';
+
+require __DIR__ . '/../resources/views/index.phtml';

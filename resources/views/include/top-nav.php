ac0eb767b524220968a5d823383db4251555c570
Lucas Brandão <lucasbranndao@gmail.com>
2025-04-22 10:32:05 -0300

    updade links top-nav

--- resources/views/include/top-nav.php
+++ resources/views/include/top-nav.php
@@ -1,10 +1,10 @@
 <nav class="bg-gray-800">
     <div class="mx-auto max-w-7xl px-4">
         <div class="flex items-center justify-start gap-4 h-16">
-            <a href="/index.php"
+            <a href="/"
                 class="nav-item text-white py-2 px-3 leading-lg rounded-lg hover:bg-gray-900 transition-all duration-[0.5s]">Home</a>
 
-            <a href="/contact.php"
+            <a href="/contact"
                 class="nav-item text-white py-2 px-3 leading-lg rounded-lg hover:bg-gray-900 transition-all duration-[0.5s]">Contact</a>
         </div>
     </div>
